c387e441a0bfbb622eab7a7f68c8ee80b50935d3
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2024-05-24 13:28:51 +0100

    CTCP-5426: Updated DataRequiredActionSpec.

--- test/controllers/actions/DataRequiredActionSpec.scala
+++ test/controllers/actions/DataRequiredActionSpec.scala
@@ -17,10 +17,11 @@
 package controllers.actions
 
 import base.{AppWithDefaultMockFixtures, SpecBase}
+import generators.Generators
 import models.requests.{DataRequest, OptionalDataRequest}
 import models.{LocalReferenceNumber, SubmissionState, UserAnswers}
-import org.scalacheck.Gen
-import org.scalatest.{Assertion, EitherValues}
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalatest.Assertion
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import play.api.mvc.Result
 import play.api.test.Helpers._
@@ -28,7 +29,7 @@ import play.api.test.Helpers._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class DataRequiredActionSpec extends SpecBase with EitherValues with AppWithDefaultMockFixtures with ScalaCheckPropertyChecks {
+class DataRequiredActionSpec extends SpecBase with AppWithDefaultMockFixtures with ScalaCheckPropertyChecks with Generators {
 
   private class Harness(lrn: LocalReferenceNumber) extends DataRequiredAction(lrn, frontendAppConfig) {
     def callRefine[A](request: OptionalDataRequest[A]): Future[Either[Result, DataRequest[A]]] = refine(request)
@@ -66,7 +67,7 @@ class DataRequiredActionSpec extends SpecBase with EitherValues with AppWithDefa
 
       "and answers have not previously been submitted" - {
         "must return Right with DataRequest" in {
-          forAll(Gen.oneOf(SubmissionState.NotSubmitted, SubmissionState.Amendment)) {
+          forAll(arbitrary[SubmissionState].retryUntil(_ != SubmissionState.Submitted)) {
             submissionStatus =>
               val userAnswers = UserAnswers(lrn = lrn, eoriNumber = eoriNumber, status = submissionStatus)
 
