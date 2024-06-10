3e12b51e0a03ef48cba3a4ea93bdecc74c0c6c3b
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2024-06-10 12:46:49 +0100

    CTCP-5501: Utilising query parameters for more performant reference data calls.

--- test/services/CountriesServiceSpec.scala
+++ test/services/CountriesServiceSpec.scala
@@ -16,37 +16,23 @@
 
 package services
 
-/*
- * Copyright 2023 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import base.SpecBase
 import cats.data.NonEmptySet
 import connectors.ReferenceDataConnector
+import connectors.ReferenceDataConnector.NoReferenceDataFoundException
 import generators.Generators
 import models.SelectableList
 import models.reference.{Country, CountryCode}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{reset, verify, when}
-import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class CountriesServiceSpec extends SpecBase with BeforeAndAfterEach with Generators {
+class CountriesServiceSpec extends SpecBase with BeforeAndAfterEach with Generators with ScalaCheckPropertyChecks {
 
   private val mockRefDataConnector: ReferenceDataConnector = mock[ReferenceDataConnector]
   private val service                                      = new CountriesService(mockRefDataConnector)
@@ -76,44 +62,32 @@ class CountriesServiceSpec extends SpecBase with BeforeAndAfterEach with Generat
       }
     }
 
-    "getCountriesWithoutZip" - {
-      "must return a list of countries without ZIP codes" in {
-
-        when(mockRefDataConnector.getCountriesWithoutZip()(any(), any()))
-          .thenReturn(Future.successful(countries.map(_.code)))
-
-        service.getCountriesWithoutZip().futureValue mustBe
-          Seq(country3.code, country2.code, country1.code)
-
-        verify(mockRefDataConnector).getCountriesWithoutZip()(any(), any())
-      }
-    }
-
     "doesCountryRequireZip" - {
       "must return true" - {
         "when countries without zip doesn't contain this country" in {
-          when(mockRefDataConnector.getCountriesWithoutZip()(any(), any()))
-            .thenReturn(Future.successful(countries.map(_.code)))
+          forAll(arbitrary[Country]) {
+            country =>
+              when(mockRefDataConnector.getCountriesWithoutZipCountry(any())(any(), any()))
+                .thenReturn(Future.successful(country.code))
 
-          val country = Arbitrary.arbitrary[Country].retryUntil(!countries.contains(_)).sample.value
+              val result = service.doesCountryRequireZip(country).futureValue
 
-          val result = service.doesCountryRequireZip(country).futureValue
-
-          result mustBe true
-
+              result mustBe true
+          }
         }
       }
 
       "must return false" - {
         "when countries without zip does contain this country" in {
-          when(mockRefDataConnector.getCountriesWithoutZip()(any(), any()))
-            .thenReturn(Future.successful(countries.map(_.code)))
+          forAll(arbitrary[Country]) {
+            country =>
+              when(mockRefDataConnector.getCountriesWithoutZipCountry(any())(any(), any()))
+                .thenReturn(Future.failed(new NoReferenceDataFoundException("")))
 
-          val country = countries.head
+              val result = service.doesCountryRequireZip(country).futureValue
 
-          val result = service.doesCountryRequireZip(country).futureValue
-
-          result mustBe false
+              result mustBe false
+          }
         }
       }
     }
