73d69d25a14edaaa64aa777c3aa77574d421e7aa
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-02-12 15:59:44 +0000

    CTCP-6265: Amended /cannot-open URL

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -46,7 +46,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
   val unauthorisedUrl: String                = s"$departureHubUrl/error/cannot-use-service-no-eori"
   val unauthorisedWithGroupAccessUrl: String = s"$departureHubUrl/unauthorised-group-access"
 
-  val lockedUrl: String = s"$departureHubUrl/cannot-open"
+  def lockedUrl(lrn: LocalReferenceNumber): String = s"$departureHubUrl/$lrn/cannot-open"
 
   val draftNotAvailableUrl: String = s"$departureHubUrl/draft-declarations/not-available"
 
