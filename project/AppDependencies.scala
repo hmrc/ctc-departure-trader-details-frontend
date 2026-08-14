822f8bc60517d415750ce2af3e87ac145cb6c24c
Eoghan-Spillane-Accenture <201935567+Eoghan-Spillane-Accenture@users.noreply.github.com>
2026-08-14 09:35:52 +0100

    DL-20031: Use service navigation by default

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,23 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.31.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.32.1",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "org.typelevel"     %% "cats-core"                              % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"               % "3.2.19",
+    "org.scalatest"           %% "scalatest"               % "3.2.20",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "org.mockito"             %  "mockito-core"            % "5.19.0",
+    "org.mockito"             %  "mockito-core"            % "5.23.0",
     "org.scalatestplus"       %% "mockito-5-12"            % "3.2.19.0",
     "org.scalacheck"          %% "scalacheck"              % "1.19.0",
     "org.scalatestplus"       %% "scalacheck-1-18"         % "3.2.19.0",
-    "org.jsoup"               %  "jsoup"                   % "1.21.2",
+    "org.jsoup"               %  "jsoup"                   % "1.23.1",
     "io.github.wolfendale"    %% "scalacheck-gen-regexp"   % "1.1.0"
   ).map(_ % "test")
 
